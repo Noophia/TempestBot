eab73bf255f82eae06a5bc9bc618541b84089ff0
Noophia <123165540+Noophia@users.noreply.github.com>
2024-06-05 22:31:10 +0300

    Update login
    api instead of password

--- credentials.js
+++ credentials.js
@@ -2,7 +2,7 @@ export const credentials =
 {
 jiraUrl: 'jira.saber3d.net',
 jiraUser: 'mtarasov',
-jiraPass: 'Mf6Uy7Hh2Wz9Rx/',
+jiraPass: 'npBD4cfNZkStIolG93FZkZAMVJKfUozGrCJ',
 };
 export const tokens =
 {
